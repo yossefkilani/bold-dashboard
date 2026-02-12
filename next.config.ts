571ce097d38dbeaf63c767116f4a1a42111baf68
yossefkilani <yossefkilani94@gmail.com>
2026-02-12 06:14:04 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: "standalone"
 };
 
 export default nextConfig;
