f6c759ec8aaf8a302400ce98d447e668911b5b4f
yossefkilani <yossefkilani94@gmail.com>
2026-02-14 20:32:32 +0300

    Update route.ts

--- app/api/submissions/[id]/route.ts
+++ app/api/submissions/[id]/route.ts
@@ -5,9 +5,9 @@ export const runtime = "nodejs";
 
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params;
 
   const db = openDB();
 
@@ -30,9 +30,9 @@ export async function GET(
 
 export async function DELETE(
   req: NextRequest,
-  context: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
-  const { id } = context.params;
+  const { id } = await context.params;
 
   const db = openDB();
 
