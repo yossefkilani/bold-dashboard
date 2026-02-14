298be59b85c924121059fa1f020990deb0ac9f9a
yossefkilani <yossefkilani94@gmail.com>
2026-02-14 20:54:50 +0300

    Update route.ts

--- app/api/notifications/route.ts
+++ app/api/notifications/route.ts
@@ -6,11 +6,9 @@ import { openDB } from "@/lib/db";
 export async function GET() {
   const db = await openDB();
 
-  const rows = await db.execute(`
-    SELECT *
-    FROM notifications
-    ORDER BY created_at DESC
-  `);
+  const [rows] = await db.execute(
+    "SELECT * FROM notifications ORDER BY created_at DESC"
+  );
 
   return NextResponse.json(rows);
 }
